f510ec7d0448751a08058e37b1a1707e192eb51d
thanlt <ks.lethanhthan@gmail.com>
2024-05-07 11:09:33 +0700

    Add new respository

---
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -0,0 +1,56 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Check_Environment {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary("C:\\Users\\admin\\AppData\\Local\\Mozilla Firefox\\firefox.exe"); //location where Firefox is installed
+        System.out.println(options.getBinary().getPath());
+        driver = new FirefoxDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_01_Url() {
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_02_Logo() {
+        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
+    }
+
+    @Test
+    public void TC_03_Form() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+    }
+
+    @AfterClass
+    public void afterClass() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
